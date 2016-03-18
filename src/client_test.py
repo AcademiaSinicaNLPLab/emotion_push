b3ee4de44f6a6700b5eb577ee16626e99d29f771
ipod825 <ipod825@gmail.com>
2016-03-18 19:03:47 +0800

    fix client test

--- src/client_test.py
+++ src/client_test.py
@@ -9,41 +9,39 @@ import sys
 
 
 preditc_text = [
-'Hello, how are you?',
-'I am ok.',
-'Please check the calander to see if you have time',
-'I am looking forward to it.',
-'I hope that you will come with me.',
-'He is expected to win the champion',
-'I can not stand with you anymore.',
-'I hate his bad behabior.',
-'He is such a jerk',
-'I fear that it will rain tomorrow',
-'He is going to kill us',
-'I will not able to pass the exam',
-'I am so glad that you come to my house',
-'We should celebrate for the team to win the game',
-'I am so excited about the coming concert.',
-'Cry, my dog just die',
-'No!!! I failed my exam.',
-'I am so lonely!!!',
-'I am so sleepy and I just want to lie on bed.',
-'I am drained out by the paper.',
-'I just can not do it anymore',
-'Vous êtes un gars mal',
+    'Hello, how are you?',
+    'I am ok.',
+    'Please check the calander to see if you have time',
+    'I am looking forward to it.',
+    'I hope that you will come with me.',
+    'He is expected to win the champion',
+    'I can not stand with you anymore.',
+    'I hate his bad behabior.',
+    'He is such a jerk',
+    'I fear that it will rain tomorrow',
+    'He is going to kill us',
+    'I will not able to pass the exam',
+    'I am so glad that you come to my house',
+    'We should celebrate for the team to win the game',
+    'I am so excited about the coming concert.',
+    'Cry, my dog just die',
+    'No!!! I failed my exam.',
+    'I am so lonely!!!',
+    'I am so sleepy and I just want to lie on bed.',
+    'I am drained out by the paper.',
+    'I just can not do it anymore',
+    'Vous êtes un gars mal',
 ]
 
 
-#model = 'sanders_svm'
-#model = 'semeval2003_bow'
-#model = 'sanders_cnn'
-model = 'panglee_svm'
-#model = 'LJ40k_bow'
-predict_d = [ {'model':model, 'text': t} for t in preditc_text ]
+model = 'MR_svm'
+# model = 'LJ40k_svm'
+predict_d = [{'model':model, 'text': t} for t in preditc_text]
 
 def parse_arg(argv):
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d', '--debug', action='store_true', help='debugging url')
+    parser.add_argument('-p', '--post', action='store_true', help='use post')
     return parser.parse_args(argv[1:])
 
 
@@ -59,8 +57,13 @@ if __name__ == "__main__":
     emotions = np.array(json.loads(requests.get(url+'listmodel').text)[model])
     for p_d in predict_d:
         print p_d['text']
-        print requests.post(url+'predict', data=json.dumps(p_d)).text
-        #pred = json.loads(requests.post(url+'predict', data=json.dumps(p_d)).text)['res']
-        #print emotions[np.argsort(np.array(pred))[::-1]]
+        if args.post:
+            res = requests.post(url+'predict', data=json.dumps(p_d)).text
+        else:
+            get_url = url+'predict'+'?model={}&text="{}"'.format(p_d['model'], p_d['text'])
+            res = requests.get(get_url).text
 
-    #print requests.post(url+'log', data=log_d).text
+        pred = json.loads(res)['res']
+        print emotions[np.argsort(np.array(pred))[::-1]], res
+
+    # print requests.post(url+'log', data=log_d).text
